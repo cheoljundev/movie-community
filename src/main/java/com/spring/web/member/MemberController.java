35e33cbdf6119cb624be09b21d06e6d8825e1b47
cheoljundev <cheoljundev@gmail.com>
2024-06-08 11:17:18 +0900

    로그인 경로 수정

--- src/main/java/com/spring/web/member/MemberController.java
+++ src/main/java/com/spring/web/member/MemberController.java
@@ -3,17 +3,12 @@ package com.spring.web.member;
 import com.spring.domain.member.Member;
 import com.spring.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
